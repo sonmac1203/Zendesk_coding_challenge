85644e9e7201aeb277daaaf6bcebffe1c5c9caea
Son Mac <83494548+sonmac1203@users.noreply.github.com>
2021-07-31 02:33:43 -0700

    Updated constants.py
    Reset default settings

---
+++ ticket_cli/constants.py
@@ -0,0 +1,9 @@
+SUBDOMAIN = 'zccsontmac1203'
+EMAIL = 'mactranthienson@gmail.com'
+API_TOKEN = 'KFZLFj5IsVFuqCT9LvuO71pzu1M5yp2yExdaCylc'
+
+# SUBDOMAIN = 'zccsonmac'
+# EMAIL = 'sontmac1203@gmail.com'
+# API_TOKEN = 'MxdnYpyczLgOpliCdvJW8pKv3SEAU3vRGzKBGHOt'
+
+PAGE_LIMIT = 25
